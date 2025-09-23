7c6fcc5f289f51b0ea4f72eeb20b509db4032ee8
imanolcier21 <92285440+imanolcier21@users.noreply.github.com>
2025-09-23 00:52:20 -0600

    Se agrega el core app para que el proyecto reconozca los endpoints de la nueva API

--- tesis_upemor/urls.py
+++ tesis_upemor/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
+    path('api/', include('core.urls')),  # Includes all URLs from the 'core' app under the '/api/' path
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')), # Catches all other requests and serves the React app
 ]
