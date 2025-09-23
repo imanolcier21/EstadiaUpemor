b64c2b635b58ca9930f23067dfa220ea74a1f867
imanolcier21 <92285440+imanolcier21@users.noreply.github.com>
2025-09-23 00:51:10 -0600

    Se agregan los middleware y las aplicaciones instaladas de cors para que funcione

--- tesis_upemor/settings.py
+++ tesis_upemor/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://192.168.1.77:3000",
 ]
 
 ROOT_URLCONF = 'tesis_upemor.urls'
