58a8d95991681ca2c772b1010f1ff5a473c3907c
imanolcier21 <92285440+imanolcier21@users.noreply.github.com>
2025-09-22 21:25:00 -0600

    Se hacen modificaciones para el funcionamiento de las migraciones

--- tesis_upemor/settings.py
+++ tesis_upemor/settings.py
@@ -132,3 +132,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom user model
+AUTH_USER_MODEL = 'core.Usuario'
