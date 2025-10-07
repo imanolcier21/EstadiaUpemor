996b117a30bf176c41e44223a3f4f335bac43fe2
imanolcier21 <92285440+imanolcier21@users.noreply.github.com>
2025-10-07 13:35:25 -0600

    Se utilizan los router para generar todas las rutas CRUD

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,18 @@
-from django.urls import path
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
+# Inicializa el Router
+router = DefaultRouter()
+# Registra el ViewSet. Esto genera: /usuarios/, /usuarios/{id}/
+router.register(r'usuarios', views.UsuarioViewSet)
+
 urlpatterns = [
+    # Rutas para el login/registro público (Estudiantes)
     path('register/', views.register_user, name='register'),
     path('login/', views.login_user, name='login'),
+    
+    # Incluye todas las rutas generadas por el Router (Gestión de Usuarios)
+    path('', include(router.urls)),
 ]
