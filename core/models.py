b3e38ffde20d42f3b7a8f2be47a84b08bee91b8c
imanolcier21 <92285440+imanolcier21@users.noreply.github.com>
2025-10-07 13:25:20 -0600

    Se agrega el modelo para agregar el tipo de usuario Super Usuario

--- core/models.py
+++ core/models.py
@@ -27,6 +27,7 @@ class UsuarioManager(BaseUserManager):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('TipoUser', 'Admin')  # Establece el tipo de usuario por defecto
 
         if extra_fields.get('is_staff') is not True:
             raise ValueError('Superuser must have is_staff=True.')
