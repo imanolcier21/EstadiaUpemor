eb30e02143ba1f28b4ea946d8d5f662108f53505
imanolcier21 <92285440+imanolcier21@users.noreply.github.com>
2025-10-13 13:49:52 -0600

    Se agregan configuraciones para el envio de crendeciales

--- tesis_upemor/settings.py
+++ tesis_upemor/settings.py
@@ -59,6 +59,20 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
 ]
 
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
 # ADD THIS NEW SETTING:
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
