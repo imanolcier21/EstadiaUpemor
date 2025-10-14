adc5ecf169d042f62add187bcae9ce1532f331a2
imanolcier21 <92285440+imanolcier21@users.noreply.github.com>
2025-10-14 12:42:34 -0600

    Se agrega el ViewSet así como el path para lar rutas generadas por el ROUTER

--- core/urls.py
+++ core/urls.py
@@ -7,12 +7,16 @@ from . import views
 router = DefaultRouter()
 # Registra el ViewSet. Esto genera: /usuarios/, /usuarios/{id}/
 router.register(r'usuarios', views.UsuarioViewSet)
+router.register(r'carreras', views.CarreraViewSet)
 
 urlpatterns = [
     # Rutas para el login/registro público (Estudiantes)
     path('register/', views.register_user, name='register'),
     path('login/', views.login_user, name='login'),
+
+    path('profile/onboard/', views.UserProfileUpdate.as_view(), name='user_profile_update'),
     
     # Incluye todas las rutas generadas por el Router (Gestión de Usuarios)
     path('', include(router.urls)),
+
 ]
