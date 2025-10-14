87ce3a3db37c2b1a57e85bd997a4f53e1de285b4
imanolcier21 <92285440+imanolcier21@users.noreply.github.com>
2025-10-14 13:44:16 -0600

    Revert "Se agregan las rutas para el onboarding de los estudiantes y la gestión de carreras"
    This reverts commit e0a66b30582fc2b7f23e8be6facb40b2e03d99a2.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,6 @@ import StudentDashboard from './components/StudentDashboard';
 import AdminDashboard from './components/AdminDashboard';
 import AdminUserManagement from './components/AdminUserManagement'; // Lo crearemos en el siguiente paso
 import './App.css';
-import StudentOnboarding from './components/StudentOnboarding';
-import AdminCareerManagement from './components/AdminCareerManagement';
 
 function App() {
   return (
@@ -18,8 +16,6 @@ function App() {
         <Route path="/dashboard/estudiante" element={<StudentDashboard />} />
         <Route path="/dashboard/admin" element={<AdminDashboard />} />
         <Route path="/admin/users" element={<AdminUserManagement />} />
-        <Route path="/onboarding/estudiante" element={<StudentOnboarding />} />
-        <Route path="/dashboard/admin/carreras" element={<AdminCareerManagement />} />
         <Route path="*" element={<AuthPage />} />
         {/* Agrega aquí más rutas como /posts, /profile, etc. */}
       </Routes>
