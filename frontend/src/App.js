1e12a218689df5db7e96d84272710bb926f0a3e3
imanolcier21 <92285440+imanolcier21@users.noreply.github.com>
2025-10-28 18:43:40 -0600

    se agregan las rutas centralizadas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,21 +8,32 @@ import AdminUserManagement from './components/AdminUserManagement'; // Lo creare
 import './App.css';
 import StudentOnboarding from './components/StudentOnboarding';
 import AdminCareerManagement from './components/AdminCareerManagement';
+import { AuthProvider  } from './context/AuthContext';
+import AdminLayout from './components/AdminLayout';
 
 function App() {
   return (
     <BrowserRouter>
+    <AuthProvider>
       <Routes>
+        {/* RUTA PÚBLICA / LOGIN */}
         <Route path="/" element={<AuthPage />} />
-
-        <Route path="/dashboard/estudiante" element={<StudentDashboard />} />
-        <Route path="/dashboard/admin" element={<AdminDashboard />} />
-        <Route path="/admin/users" element={<AdminUserManagement />} />
         <Route path="/onboarding/estudiante" element={<StudentOnboarding />} />
-        <Route path="/dashboard/admin/carreras" element={<AdminCareerManagement />} />
+        
+        {/* RUTA CENTRALIZADA: Un solo Layout que maneja TODO el contenido después del login */}
+        <Route element={<AdminLayout />}>
+            {/* 1. Dashboard Principal (Ruta: /dashboard/admin) */}
+            <Route path="/dashboard/admin" element={<AdminDashboard />} />
+            {/* 2. CRUDs de Gestión (Rutas: /admin/users, /admin/carreras) */}
+            <Route path="/admin/users" element={<AdminUserManagement />} />
+            <Route path="/admin/carreras" element={<AdminCareerManagement />} />
+            {/* 3. Dashboard Estudiante */}
+            <Route path="/dashboard/estudiante" element={<StudentDashboard />} />
+        </Route>
+        
         <Route path="*" element={<AuthPage />} />
-        {/* Agrega aquí más rutas como /posts, /profile, etc. */}
       </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
