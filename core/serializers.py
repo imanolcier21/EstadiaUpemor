89f87054f3d3d64ad0e7e80f917ec66b12b0c7cf
imanolcier21 <92285440+imanolcier21@users.noreply.github.com>
2025-10-29 23:49:51 -0600

    Se agrega el serializador post para pooderguardar y mostrar las imagenes correctamente de las publicaciones

--- core/serializers.py
+++ core/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Usuario
 from .models import Carrera
+from .models import Post
 
 class UsuarioSerializer(serializers.ModelSerializer):
     # Campo personalizado para la relación de Carrera (mostrar el nombre, no el ID)
@@ -21,3 +22,35 @@ class CarreraSerializer(serializers.ModelSerializer):
         model = Carrera
         fields = ['idCarrera', 'NomCarrera']
         read_only_fields = ('idCarrera',)
+
+class PostSerializer(serializers.ModelSerializer):
+    autor = serializers.CharField(source='idUser.UserName', read_only=True)
+    media_url = serializers.SerializerMethodField(read_only=True)
+    idUser = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = Post
+        fields = [
+            'idPost', 'TextPost', 'MediaPost', 'media_url', 'FechCreacPost', 'FechUpdatePost',
+            'idUser', 'autor', 'idGrupo', 'SentimientoScore', 'SentimientoLabel'
+        ]
+        read_only_fields = (
+            'idPost', 'FechCreacPost', 'FechUpdatePost', 'autor', 'media_url',
+            'SentimientoScore', 'SentimientoLabel', 'idUser'
+        )
+
+    def get_media_url(self, obj):
+        request = self.context.get('request')
+        if obj.MediaPost:
+            url = obj.MediaPost.url
+            if request:
+                return request.build_absolute_uri(url)
+            return url
+        return None
+
+    def validate_MediaPost(self, value):
+        if value:
+            content_type = value.content_type
+            if not (content_type.startswith('image/') or content_type.startswith('video/')):
+                raise serializers.ValidationError('Solo se permiten imágenes o videos.')
+        return value
