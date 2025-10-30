459a787084aa2a2a056ac92ce216514dae5ca9d9
imanolcier21 <92285440+imanolcier21@users.noreply.github.com>
2025-10-30 00:02:42 -0600

    se agrega funcinamiento al urlpatterns para que muestre las imágenes correctamente

--- tesis_upemor/urls.py
+++ tesis_upemor/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # Asegúrate de que esta línea esté primero
     path('api/', include('core.urls')),
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
