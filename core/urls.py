21cb50c90cec8407ad6e0bd657765bd6dd45bcac
imanolcier21 <92285440+imanolcier21@users.noreply.github.com>
2025-11-06 23:16:35 -0600

    se agregan las urls para los reportes

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
-from .views import PasswordResetRequestAPIView, PasswordResetConfirmAPIView, MiPerfilView, PerfilPublicoView, ExplorarUsuariosView
+from .views import PasswordResetRequestAPIView, PasswordResetConfirmAPIView, MiPerfilView, PerfilPublicoView, ExplorarUsuariosView, MisNotificacionesView, ChatbotAPIView, AlertaIAAPIView, backup_import
 
 # Inicializa el Router
 router = DefaultRouter()
@@ -13,6 +13,9 @@ router.register(r'carreras', views.CarreraViewSet)
 router.register(r'posts', views.PostViewSet)
 router.register(r'comentarios', views.ComentarioViewSet)
 router.register(r'mensajes', views.MensajeDirectoViewSet)
+router.register(r'grupos', views.GrupoViewSet)
+router.register(r'eventos', views.EventoViewSet)
+router.register(r'recursos_apoyo', views.RecursoApoyoViewSet)
 
 urlpatterns = [
     # Rutas para el login/registro público (Estudiantes)
@@ -26,8 +29,25 @@ urlpatterns = [
     path('perfil/mio/', MiPerfilView.as_view(), name='mi_perfil'),
     path('perfil/<str:username>/', PerfilPublicoView.as_view(), name='perfil_publico'),
     path('usuarios/explorar/', ExplorarUsuariosView.as_view(), name='explorar_usuarios'),
+    path('notificaciones/mis/', MisNotificacionesView.as_view(), name='mis_notificaciones'),
+    path('chatbot/', ChatbotAPIView.as_view(), name='chatbot'),
+    path('dummy/', views.test_dummy),
     
     # Incluye todas las rutas generadas por el Router (Gestión de Usuarios)
     path('', include(router.urls)),
 
+]
+
+urlpatterns += [
+    path('alertas_ia/', AlertaIAAPIView.as_view()),
+    path('reportes/actividad_usuarios/', views.ReporteActividadUsuariosView.as_view()),
+    path('reportes/sentimiento_publicaciones/', views.ReporteSentimientoPublicacionesView.as_view()),
+    path('reportes/interacciones_chatbot/', views.ReporteInteraccionesChatbotView.as_view()),
+    path('reportes/recursos_apoyo/', views.ReporteRecursosApoyoView.as_view()),
+    path('reportes/grupos_activos/', views.ReporteGruposActivosView.as_view()),
+    path('reportes/eventos_mayor_asistencia/', views.ReporteEventosMayorAsistenciaView.as_view()),
+    path('reportes/tendencias_carrera/', views.ReporteTendenciasCarreraView.as_view()),
+    path('reportes/notificaciones/', views.ReporteNotificacionesView.as_view()),
+    path('backup/export/', views.BackupExportView.as_view()),
+    path('backup/import/', backup_import),
 ]
